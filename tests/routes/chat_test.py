c1e063f00d265dc999dba55383bd6d87f2d1293d
chomookun <chomookun@gmail.com>
2024-05-24 01:53:02 +0900

    organizes test case

--- tests/routes/chat_test.py
+++ tests/routes/chat_test.py
@@ -2,19 +2,19 @@ import unittest
 import sys
 import os
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'src')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..', '..', 'src')))
 
 
 from finticsai.app import app
 
-class FinticsMlTestCase(unittest.TestCase):
+class ChatTestCase(unittest.TestCase):
     def setUp(self):
         print("setUp")
         self.client = app.test_client()
         app.config['TESTING'] = True
 
     def test_get_asset(self):
-        response = self.client.get('/assets/123')
+        response = self.client.get('/chat')
         print(response.json)
         self.assertEqual(response.status_code, 200)
 
---
+++ tests/routes/news_test.py
@@ -0,0 +1,22 @@
+import unittest
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..', '..', 'src')))
+
+
+from finticsai.app import app
+
+class NewsTestCase(unittest.TestCase):
+    def setUp(self):
+        print("setUp")
+        self.client = app.test_client()
+        app.config['TESTING'] = True
+
+    def test_get_asset(self):
+        response = self.client.get('/news')
+        print(response.json)
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
