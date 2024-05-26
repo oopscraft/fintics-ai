5e2042e40fd7f636116f1872251d45468478808c
chomookun <chomookun@gmail.com>
2024-05-26 22:57:54 +0900

    add environment variable OLLAMA_BASE_URL, OLLAMA_MODEL

--- finticsai/modules/ollama.py
+++ finticsai/modules/ollama.py
@@ -2,5 +2,6 @@ from langchain_community.chat_models import ChatOllama
 import os
 
 # ollama
-ollama_url = os.getenv('OLLAMA_URL', "http://127.0.0.1:11434")
-chatOllama = ChatOllama(model = "gemma:2b", base_url = ollama_url)
+ollama_base_url = os.getenv('OLLAMA_BASE_URL', "http://127.0.0.1:11434")
+ollama_model = os.getenv('OLLAMA_MODEL', "gemma:2b")
+chatOllama = ChatOllama(model=ollama_model, base_url=ollama_base_url)
